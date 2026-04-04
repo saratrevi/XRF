2ce4311e84aa13da298142868cd1a609bdad0436
ALUnibo <alberto.luise@studio.unibo.it>
2026-04-04 15:00:05 +0200

    Inittial edit and MVC

--- src/xrf_model/physics.py
+++ src/xrf_model/physics.py
@@ -1,7 +1,163 @@
 # src/xrf_model/physics.py
 from __future__ import annotations
 
+from typing import Iterable
+
 import numpy as np
+from scipy.interpolate import interp1d
+
+import pandas as pd
+import os
+from .io import (
+    load_element_mass_absorption_data,
+    interpolate_element_full_data,
+    load_form_factor_data,
+)
+
+class ElementProperties:
+    
+    def __init__(self, cfg: dict):
+        self.cfg = cfg
+
+        df = pd.read_excel(cfg["paths"]["element_data"], index_col=0)
+        self.df = df.loc[:, :'mantle_mars'] # NOTE keep only non-concentration columns
+
+        # Retrieve element properties
+        self.param_dict = {}
+        for el in cfg["all_matrix_elements"]:
+            pdata = {
+                "f_rel": self.df.at[el, "f_REL"],
+                "fNT": self.df.at[el, "f_NT"],
+                "Atomic Number": self.df.at[el, "Atomic Number"],
+                "Atomic Mass": self.df.at[el, "Atomic Mass"],
+            }
+            self.param_dict[el] = pdata
+
+        # Absorption data
+        # --- Load & interpolate mass absorption data 
+        self.element_xs_data = {}
+        data_dir = self.cfg["paths"]["absorption_dir"]
+        for el in cfg["all_matrix_elements"]:
+            txt_path = os.path.join(data_dir, f"{el}.txt")
+            if not os.path.exists(txt_path):
+                raise FileNotFoundError(f"Missing element file: {txt_path}")
+            self.element_xs_data[el] = load_element_mass_absorption_data(txt_path)
+
+        # Form factor data
+        self.form_factor_dict = load_form_factor_data(cfg["paths"]["form_factors"])
+
+    def realign(self, common_energy):
+        self.full_data_dict = {
+            el: interpolate_element_full_data(xs_data, common_energy)
+            for el, xs_data in self.element_xs_data.items()
+        }
+
+
+def compute_fluorescence_spectrum(conc,
+                                  conc_df,
+                                  energy_solar_flare,
+                                  concentrations : dict[str, float],
+                                  element_properties : ElementProperties,
+                                  flux_solar_flare):
+    footprint = element_properties.cfg["footprint"]
+    distance_sun_AU = element_properties.cfg["distance_sun_AU"]
+
+    theta = element_properties.cfg["theta"]  # NOTE: incidence angle w.r.t. the normal vector (which is the POV)
+    phi = element_properties.cfg["phi"]  # NOTE: angle of emission w.r.t. the normal vector
+    solid_angle = 1 / (element_properties.cfg["altitude"] ** 2)  # NOTE: we could do everything with this, possibly skipping altitude
+    scattering_angle = np.pi - (theta + phi)
+
+    sample_mass_atten_array = np.zeros_like(energy_solar_flare, dtype=float)
+    for el_name, w in concentrations.items():
+        if w > 0:
+            sample_mass_atten_array += (
+                w * element_properties.full_data_dict[el_name]["total_mass_attenuation_interp"]
+            )
+    scale = (footprint * np.cos(theta)) / (distance_sun_AU ** 2) # NOTE: we'll need to either change this (for now: just move it ealiers)
+    flux_scaled = flux_solar_flare * scale * solid_angle
+
+    I_coh = compute_coherent_scattering_sample(
+        energy_array=energy_solar_flare,
+        flux_array=flux_scaled,
+        sample_mass_atten_array=sample_mass_atten_array,
+        param_dict=element_properties.param_dict,
+        form_factor_dict=element_properties.form_factor_dict,
+        full_data_dict=element_properties.full_data_dict,
+        concentration_key=conc,
+        conc_df=conc_df,
+        scattering_angle=scattering_angle,
+        theta=theta,
+        phi=phi,
+    )
+
+    fluorescence_rows = []
+    f_atten_interp = interp1d(energy_solar_flare, sample_mass_atten_array,
+                              bounds_error=False, fill_value="extrapolate")
+
+    for el, params in element_properties.param_dict.items():
+        w = conc_df[conc][el]
+        if w <= 0 or el not in element_properties.cfg["fluorescence_elements"]:
+            continue
+
+        
+        E_edge = float(element_properties.df.at[el, "Absorption Edge"])
+        wK = float(element_properties.df.at[el, "Fluorescence Yield"])
+        R  = float(element_properties.df.at[el, "r-1/r"])
+
+        # --- Kα ---
+        E_Ka = float(element_properties.df.at[el, "K-Alpha Line"])
+        P_Ka = float(element_properties.df.at[el, "P_kalpha"])
+        mu_Ka = float(f_atten_interp(E_Ka))
+
+        if E_edge > 0 and E_Ka > 0 and P_Ka > 0:
+            raw_Ka = compute_k_fluorescence(
+            energy_array=energy_solar_flare,
+            flux_array=flux_scaled,
+            photoelectric_array_element=element_properties.full_data_dict[el]["photoelectric_absorption_interp"],
+            total_atten_sample_array=sample_mass_atten_array,
+            total_atten_sample_at_line=mu_Ka,
+            absorption_edge=E_edge,
+            theta=theta,
+            phi=phi,
+            )
+            I_Ka = (raw_Ka * w * wK * R * P_Ka) / (4 * np.pi)
+        else:
+            I_Ka = 0.0
+        
+        # NOTE: this is already output code
+        fluorescence_rows.append({
+            "Element": el, "Concentration": w, "Line": "Kα",
+            "E_line_keV": E_Ka, "P_line": P_Ka, "Intensity_ph_cm2_s": I_Ka
+        })
+
+        # --- Kβ (optional if present)
+        E_Kb = float(element_properties.df.at[el, "K-beta Line"])
+        P_Kb = float(element_properties.df.at[el, "P_kbeta"])
+        if E_Kb > 0.0 and P_Kb > 0.0:
+            mu_Kb = float(f_atten_interp(E_Kb))
+            raw_Kb = compute_k_fluorescence(
+                energy_array=energy_solar_flare,
+                flux_array=flux_scaled,
+                photoelectric_array_element=element_properties.full_data_dict[el]["photoelectric_absorption_interp"],
+                total_atten_sample_array=sample_mass_atten_array,
+                total_atten_sample_at_line=mu_Kb,
+                absorption_edge=E_edge,
+                theta=theta,
+                phi=phi,
+            )
+            I_Kb = (raw_Kb * w * wK * R * P_Kb) / (4 * np.pi)
+        else:
+            I_Kb = 0.0
+
+        # NOTE: output code
+        fluorescence_rows.append({
+            "Element": el, "Concentration": w, "Line": "Kβ",
+            "E_line_keV": E_Kb if E_Kb > 0 else np.nan,
+            "P_line": P_Kb, "Intensity_ph_cm2_s": I_Kb
+        })
+
+        return {"rows": fluorescence_rows, "icoh": I_coh}
+
 
 def compute_coherent_scattering_sample(
     energy_array,
@@ -10,6 +166,7 @@ def compute_coherent_scattering_sample(
     param_dict,
     form_factor_dict,
     full_data_dict,
+    conc_df,
     concentration_key,
     scattering_angle,
     theta,
@@ -70,7 +227,8 @@ def compute_coherent_scattering_sample(
             # Differential cross section per atom:
             dsigma = ((r_e**2)/2.0) * ang_factor * (f_eff**2)
             # Weight by mass fraction and atoms per gram:
-            sigma_eff = param_dict[el_name][concentration_key] * (N_A / param_dict[el_name]["Atomic Mass"]) * dsigma
+            sigma_eff = conc_df[concentration_key][el_name] * (N_A / param_dict[el_name]["Atomic Mass"]) * dsigma
+            # param_dict[el_name][concentration_key]
             sigma_coh_sample += sigma_eff
         else:
             print(f"Warning: Missing data for element {el_name}. Skipping.")
@@ -107,4 +265,5 @@ def compute_k_fluorescence(
     geometry_factor = np.cos(theta) / np.cos(phi)
     denom = mu_tot_E + (geometry_factor*total_atten_sample_at_line)
     integrand = np.where(denom > 0.0, flux_sub * mu_ph / denom, 0.0)
-    return np.trapz(integrand, x=E_sub)
+    # return np.trapz(integrand, x=E_sub)
+    return np.trapezoid(integrand, x=E_sub)
--- src/xrf_model/pipeline.py
+++ src/xrf_model/pipeline.py
@@ -5,15 +5,18 @@ import os
 from pathlib import Path
 import numpy as np
 import pandas as pd
-from scipy.interpolate import interp1d
+# from scipy.interpolate import interp1d
+
+# NOTE: a useful library: xraylib
 
 from .io import (
     load_flux_data,
-    load_element_mass_absorption_data,
-    interpolate_element_full_data,
-    load_form_factor_data,
+    # load_element_mass_absorption_data,
+    # interpolate_element_full_data,
+    # load_form_factor_data,
 )
 from .physics import compute_coherent_scattering_sample, compute_k_fluorescence
+from .physics import ElementProperties, compute_fluorescence_spectrum
 from .output import save_coherent_spectrum_txt
 
 def safe_float(x, default=0.0) -> float:
@@ -39,48 +42,62 @@ def run_one(flux_file: str | Path, cfg: dict) -> dict:
 
     # --- Load element parameters
     df = pd.read_excel(cfg["paths"]["element_data"], index_col=0)
+    df = df.loc[:, 'mantle_mars':] # NOTE discard all non-concentration columns
+
     concentration_cols = cfg["concentration_cols"]
 
-    theta = cfg["theta"]
-    phi = cfg["phi"]
-    altitude = cfg["altitude"]
-    distance_sun_AU = cfg["distance_sun_AU"]
-    footprint = cfg["footprint"]
+    # theta = cfg["theta"] # NOTE: incidence angle w.r.t. the normal vector (which is the POV)
+    # phi = cfg["phi"] # NOTE: angle of emission w.r.t. the normal vector
+    # altitude = cfg["altitude"] # NOTE: this should become our main distance
+    # distance_sun_AU = cfg["distance_sun_AU"] # NOTE: this should not matter in the in-situ setting
+    # footprint = cfg["footprint"] # NOTE: this will represent the area of a grid cell
 
     # Your code uses 1/altitude^2 as "solid angle" factor
-    solid_angle = 1 / (altitude ** 2)
-    scattering_angle = np.pi - (theta + phi)
+    # solid_angle = 1 / (altitude ** 2) # NOTE: we could do everything with this, possibly skipping altitude
+    # scattering_angle = np.pi - (theta + phi) # NOTE: this should be correct
 
-    # --- Build param_dict
-    param_dict = {}
+    # # --- Build param_dict
+    # param_dict = {}
+    # for el in cfg["all_matrix_elements"]:
+    #     pdata = { # NOTE: this are all fixed properties of elements
+    #         "f_rel": df.at[el, "f_REL"],
+    #         "fNT": df.at[el, "f_NT"],
+    #         "Atomic Number": df.at[el, "Atomic Number"],
+    #         "Atomic Mass": df.at[el, "Atomic Mass"],
+    #     }
+    #     for key in concentration_cols: # NOTE: this will become a primary input (they are currently the last cols in Data_keV.xlsx)
+    #         pdata[key] = df.at[el, key]
+    #     param_dict[el] = pdata
+
+    # Retrieve the cocentrations
+    concentrations = {conc:{} for conc in concentration_cols}
     for el in cfg["all_matrix_elements"]:
-        pdata = {
-            "f_rel": df.at[el, "f_REL"],
-            "fNT": df.at[el, "f_NT"],
-            "Atomic Number": df.at[el, "Atomic Number"],
-            "Atomic Mass": df.at[el, "Atomic Mass"],
-        }
         for key in concentration_cols:
-            pdata[key] = df.at[el, key]
-        param_dict[el] = pdata
+            concentrations[key][el] = df.at[el, key]
+    
+    # Init the element property class
+    element_properties = ElementProperties(cfg)
 
-    # --- Load & interpolate mass absorption data
-    element_xs_data = {}
-    data_dir = cfg["paths"]["absorption_dir"]
-    for el in param_dict:
-        txt_path = os.path.join(data_dir, f"{el}.txt")
-        if not os.path.exists(txt_path):
-            raise FileNotFoundError(f"Missing element file: {txt_path}")
-        element_xs_data[el] = load_element_mass_absorption_data(txt_path)
+    # # NOTE: these are other element properties (curves of different energy-dependent properties)
+    # # --- Load & interpolate mass absorption data 
+    # element_xs_data = {}
+    # data_dir = cfg["paths"]["absorption_dir"]
+    # for el in param_dict:
+    #     txt_path = os.path.join(data_dir, f"{el}.txt")
+    #     if not os.path.exists(txt_path):
+    #         raise FileNotFoundError(f"Missing element file: {txt_path}")
+    #     element_xs_data[el] = load_element_mass_absorption_data(txt_path)
 
-    common_energy = energy_solar_flare.copy()
-    full_data_dict = {
-        el: interpolate_element_full_data(xs_data, common_energy)
-        for el, xs_data in element_xs_data.items()
-    }
+    # NOTE: this describes the illumination; the flux is an energy-dependent curve; this is another primary input
+    # full_data_dict = {
+    #     el: interpolate_element_full_data(xs_data, common_energy)
+    #     for el, xs_data in element_xs_data.items()
+    # }
+    element_properties.realign(energy_solar_flare)
 
+    # NOTE: these are other element properties that depend on the energy-time-angle scalar
     # --- Load form factors
-    form_factor_dict = load_form_factor_data(cfg["paths"]["form_factors"])
+    # form_factor_dict = load_form_factor_data(cfg["paths"]["form_factors"])
 
     # --- Output dir per flare (prevents overwriting)
     out_dir = Path(cfg["paths"]["output_dir"]) / flare_name
@@ -88,111 +105,134 @@ def run_one(flux_file: str | Path, cfg: dict) -> dict:
 
     results_dict = {}
 
+    # NOTE: here we computing terms that will later used for the integration
     # --- Loop concentrations
     for conc in concentration_cols:
-        sample_mass_atten_array = np.zeros_like(common_energy, dtype=float)
-        for el_name, params in param_dict.items():
-            w = params[conc]
-            if w > 0:
-                sample_mass_atten_array += (
-                    w * full_data_dict[el_name]["total_mass_attenuation_interp"]
-                )
 
-        scale = (footprint * np.cos(theta)) / (distance_sun_AU ** 2)
-        flux_scaled = flux_solar_flare_0 * scale * solid_angle
+        print('='*78)
+        print(f'SAMPLE: {conc}')
+        print('='*78)
 
-        I_coh = compute_coherent_scattering_sample(
-            energy_array=common_energy,
-            flux_array=flux_scaled,
-            sample_mass_atten_array=sample_mass_atten_array,
-            param_dict=param_dict,
-            form_factor_dict=form_factor_dict,
-            full_data_dict=full_data_dict,
-            concentration_key=conc,
-            scattering_angle=scattering_angle,
-            theta=theta,
-            phi=phi,
-        )
+        sample_concentrations = concentrations[conc]
 
-        fluorescence_rows = []
-        f_atten_interp = interp1d(common_energy, sample_mass_atten_array,
-                                  bounds_error=False, fill_value="extrapolate")
+        # sample_mass_atten_array = np.zeros_like(common_energy, dtype=float)
+        # for el_name, params in param_dict.items():
+        #     w = params[conc]
+        #     if w > 0:
+        #         sample_mass_atten_array += (
+        #             w * full_data_dict[el_name]["total_mass_attenuation_interp"]
+        #         )
 
-        for el, params in param_dict.items():
-            w = params[conc]
-            if w <= 0 or el not in cfg["fluorescence_elements"]:
-                continue
 
-            
-            E_edge = safe_float(df.at[el, "Absorption Edge"], default=0.0)
-            wK = safe_float(df.at[el, "Fluorescence Yield"], default=0.0)
-            R  = safe_float(df.at[el, "r-1/r"], default=0.0)
+        #scale = (footprint * np.cos(theta)) / (distance_sun_AU ** 2) # NOTE: we'll need to either change this (for now: just move it ealiers)
+        #flux_scaled = flux_solar_flare_0 * scale * solid_angle
+
+        #I_coh = compute_coherent_scattering_sample(
+        #    energy_array=common_energy,
+        #    flux_array=flux_scaled,
+        #    sample_mass_atten_array=sample_mass_atten_array,
+        #    param_dict=param_dict,
+        #    form_factor_dict=form_factor_dict,
+        #    full_data_dict=full_data_dict,
+        #    concentration_key=conc,
+        #    scattering_angle=scattering_angle,
+        #    theta=theta,
+        #    phi=phi,
+        #)
+
+        #fluorescence_rows = []
+        #f_atten_interp = interp1d(common_energy, sample_mass_atten_array,
+        #                          bounds_error=False, fill_value="extrapolate")
+
+        #for el, params in param_dict.items():
+        #    w = params[conc]
+        #    if w <= 0 or el not in cfg["fluorescence_elements"]:
+        #        continue
+
+        
+        #    E_edge = safe_float(df.at[el, "Absorption Edge"], default=0.0)
+        #    wK = safe_float(df.at[el, "Fluorescence Yield"], default=0.0)
+        #    R  = safe_float(df.at[el, "r-1/r"], default=0.0)
 
             # --- Kα ---
-            E_Ka = safe_float(df.at[el, "K-Alpha Line"], default=0.0)
-            P_Ka = safe_float(df.at[el, "P_kalpha"], default=0.0)
-            mu_Ka = float(f_atten_interp(E_Ka))
+        #    E_Ka = safe_float(df.at[el, "K-Alpha Line"], default=0.0)
+        #    P_Ka = safe_float(df.at[el, "P_kalpha"], default=0.0)
+        #    mu_Ka = float(f_atten_interp(E_Ka))
 
-            if E_edge > 0 and E_Ka > 0 and P_Ka > 0:
-                raw_Ka = compute_k_fluorescence(
-                energy_array=common_energy,
-                flux_array=flux_scaled,
-                photoelectric_array_element=full_data_dict[el]["photoelectric_absorption_interp"],
-                total_atten_sample_array=sample_mass_atten_array,
-                total_atten_sample_at_line=mu_Ka,
-                absorption_edge=E_edge,
-                theta=theta,
-                phi=phi,
-                )
-                I_Ka = (raw_Ka * w * wK * R * P_Ka) / (4 * np.pi)
-            else:
-                I_Ka = 0.0
-
-            fluorescence_rows.append({
-                "Element": el, "Concentration": w, "Line": "Kα",
-                "E_line_keV": E_Ka, "P_line": P_Ka, "Intensity_ph_cm2_s": I_Ka
-            })
-
+        #    if E_edge > 0 and E_Ka > 0 and P_Ka > 0:
+        #        raw_Ka = compute_k_fluorescence(
+        #        energy_array=common_energy,
+        #        flux_array=flux_scaled,
+        #        photoelectric_array_element=full_data_dict[el]["photoelectric_absorption_interp"],
+        #        total_atten_sample_array=sample_mass_atten_array,
+        #        total_atten_sample_at_line=mu_Ka,
+        #        absorption_edge=E_edge,
+        #        theta=theta,
+        #        phi=phi,
+        #        )
+        #        I_Ka = (raw_Ka * w * wK * R * P_Ka) / (4 * np.pi)
+        #    else:
+        #        I_Ka = 0.0
+            
+            # NOTE: this is already output code
+        #    fluorescence_rows.append({
+        #        "Element": el, "Concentration": w, "Line": "Kα",
+        #        "E_line_keV": E_Ka, "P_line": P_Ka, "Intensity_ph_cm2_s": I_Ka
+        #    })
+        
             # --- Kβ (optional if present)
-            E_Kb = safe_float(df.at[el, "K-beta Line"], default=0.0)
-            P_Kb = safe_float(df.at[el, "P_kbeta"], default=0.0)
-            if E_Kb > 0.0 and P_Kb > 0.0:
-                mu_Kb = float(f_atten_interp(E_Kb))
-                raw_Kb = compute_k_fluorescence(
-                    energy_array=common_energy,
-                    flux_array=flux_scaled,
-                    photoelectric_array_element=full_data_dict[el]["photoelectric_absorption_interp"],
-                    total_atten_sample_array=sample_mass_atten_array,
-                    total_atten_sample_at_line=mu_Kb,
-                    absorption_edge=E_edge,
-                    theta=theta,
-                    phi=phi,
-                )
-                I_Kb = (raw_Kb * w * wK * R * P_Kb) / (4 * np.pi)
-            else:
-                I_Kb = 0.0
+        #    E_Kb = safe_float(df.at[el, "K-beta Line"], default=0.0)
+        #    P_Kb = safe_float(df.at[el, "P_kbeta"], default=0.0)
+        #    if E_Kb > 0.0 and P_Kb > 0.0:
+        #        mu_Kb = float(f_atten_interp(E_Kb))
+        #        raw_Kb = compute_k_fluorescence(
+        #            energy_array=common_energy,
+        #            flux_array=flux_scaled,
+        #            photoelectric_array_element=full_data_dict[el]["photoelectric_absorption_interp"],
+        #            total_atten_sample_array=sample_mass_atten_array,
+        #            total_atten_sample_at_line=mu_Kb,
+        #            absorption_edge=E_edge,
+        #            theta=theta,
+        #            phi=phi,
+        #        )
+        #        I_Kb = (raw_Kb * w * wK * R * P_Kb) / (4 * np.pi)
+        #    else:
+        #        I_Kb = 0.0
 
-            fluorescence_rows.append({
-                "Element": el, "Concentration": w, "Line": "Kβ",
-                "E_line_keV": E_Kb if E_Kb > 0 else np.nan,
-                "P_line": P_Kb, "Intensity_ph_cm2_s": I_Kb
-            })
+            # NOTE: output code
+        #    fluorescence_rows.append({
+        #        "Element": el, "Concentration": w, "Line": "Kβ",
+        #        "E_line_keV": E_Kb if E_Kb > 0 else np.nan,
+        #        "P_line": P_Kb, "Intensity_ph_cm2_s": I_Kb
+        #    })
 
-        df_fluo = pd.DataFrame(fluorescence_rows).sort_values(["Element", "Line"])
+        
+        results = compute_fluorescence_spectrum(
+            conc=conc,
+            conc_df=df,
+            energy_solar_flare=energy_solar_flare,
+            concentrations=sample_concentrations,
+            element_properties=element_properties,
+            flux_solar_flare=flux_solar_flare_0,
+
+        )
+
+        # NOTE: output code
+        df_fluo = pd.DataFrame(results["rows"]).sort_values(["Element", "Line"])
 
         # Save outputs (same as your notebook but inside results/<cfg>/<flare>/)
         xlsx_path = out_dir / f"{flare_name}_{conc}_results.xlsx"
         with pd.ExcelWriter(xlsx_path) as writer:
-            pd.DataFrame({"Energy": common_energy, "I_coherent": I_coh}).to_excel(
+            pd.DataFrame({"Energy": energy_solar_flare, "I_coherent": results["icoh"]}).to_excel(
                 writer, sheet_name="Coherent", index=False
             )
             df_fluo.to_excel(writer, sheet_name="Fluorescence", index=False)
 
-        save_coherent_spectrum_txt(flare_name, conc, common_energy, I_coh, out_dir=str(out_dir))
+        save_coherent_spectrum_txt(flare_name, conc, energy_solar_flare, results["icoh"], out_dir=str(out_dir))
 
         results_dict[conc] = {
-            "energy": common_energy,
-            "I_coherent": I_coh,
+            "energy": energy_solar_flare,
+            "I_coherent": results["icoh"],
             "fluorescence": df_fluo,
             "out_dir": str(out_dir),
         }
